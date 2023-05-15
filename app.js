9eb63a281380a252810431eab532cfe89d41219d
anishsingh007 <emailanishsingh@gmail.com>
2023-05-15 15:36:21 +0530

    Added ProductPage with SIze Button

--- app.js
+++ app.js
@@ -1,16 +1,34 @@
 const http = require("http");
 
 const express =require("express");
+const bodyParser = require("body-parser");
 
 const app = express();
-app.use((req,res,next)=>{
+
+app.use(bodyParser.urlencoded({extended:false}));
+
+app.use('/add-product',(req,res,next)=>{
     console.log(`ds`);
-    next();    
+    res.send(`
+    <form action="/product" method="POST">
+      <input type="text" name="title" placeholder="Product Title" required><br>
+      <input type="number" name="size" placeholder="Product Size" required><br>
+      <button type="submit">Add Product</button>
+    </form>
+  `);
+});
+
+app.post('/product',(req,res,next)=>{
+    console.log(req.body);
+    res.redirect('/');
+    
+        
 });
 
 app.use((req,res,next)=>{
     console.log("kaksaka");
-    res.send('<h1> hello to node js </h1>')
+    res.send('<h1> hello From Express </h1>');
+    
 })
 
 
